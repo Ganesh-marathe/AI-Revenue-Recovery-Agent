594584fc09a62e7dbdef356309c6f71acfdca5ab
Ganesh Marathe <ganeshmarathe361@gmail.com>
2026-09-05 22:43:50 +0530

    Fix authentication API port

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "http://127.0.0.1:8001";
 
 function Login({ onLogin }) {
   const [mode, setMode] = useState("login");
